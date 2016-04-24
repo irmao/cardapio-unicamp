4810dbb936ada98c8765c174ee0f7d152dc25ec0
irmao <v.fernandesdias@gmail.com>
2016-04-23 22:30:31 -0300

    Integrating HTTP Server with the menu reading function

--- MenuParser.py
+++ MenuParser.py
@@ -8,17 +8,17 @@ from StateMachineHandler import StateMachineHandler
 class MenuParser(HTMLParser):
     _openTags = Stack()
     _stateMachine = MenuStateMachine()
+    _output_buffer = ''
     
     def get_open_tag(self):
         return self._openTags.top()
     
     def handle_starttag(self, tag, attrs):
-        #print tag, StateMachineHandler.get_current_lookup_tag(self._stateMachine.get_current_state())
         if StateMachineHandler.match_open_tag(tag, self._stateMachine.get_current_state()):
             self._openTags.push(tag)
             state = self._stateMachine.get_current_state()
             if  state > MenuStateMachine.STATE_TITLES_TABLE and state < MenuStateMachine.STATE_DONE:
-                print "-------------------------%s-------------------------" % StateMachineHandler.to_string(state)
+                self._output_buffer += "-------------------------%s-------------------------\n" % StateMachineHandler.to_string(state)
             self._stateMachine.go_to_next_state()
 
     def handle_endtag(self, tag):
@@ -29,7 +29,10 @@ class MenuParser(HTMLParser):
         if state > MenuStateMachine.STATE_TITLES_TABLE+1 and state < MenuStateMachine.STATE_DONE+1:
             data = data.strip()
             if data and not data.isspace():
-                print data
+                self._output_buffer += data + '\n'
+                
+    def get_output_data(self):
+        return self._output_buffer
         
 
 def testMenuParser():
--- app.py
+++ app.py
@@ -1,12 +1,27 @@
+#!/usr/bin/python
+
+import urllib2
+from MenuParser import MenuParser
+
+MENU_URL = r'http://catedral.prefeitura.unicamp.br/cardapio.php'
+
+def get_menu():
+    parser = MenuParser()
+    response = urllib2.urlopen(MENU_URL)
+    html = response.read()
+    parser.feed(html)
+    output_data = parser.get_output_data()
+    return output_data
+
 def wsgi_app(environ, start_response):
     status = '200 OK'
     response_headers = [('Content-type', 'text/plain')]
     start_response(status, response_headers)
-    response_body = 'Hello World'
-    yield response_body.encode()
+    response_body = get_menu()
+    yield response_body
 
 if __name__ == '__main__':
     from wsgiref.simple_server import make_server
 
-    httpd = make_server('localhost', 5555, wsgi_app)
+    httpd = make_server('localhost', 80, wsgi_app)
     httpd.serve_forever()
