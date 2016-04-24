d18285a6c1ebdf430fb96f7060aa705b61aea206
irmao <v.fernandesdias@gmail.com>
2016-04-23 21:09:14 -0300

    Renaming Server.py to app.py as this is an Azure requirement

--- app.py
+++ app.py
@@ -8,5 +8,5 @@ def wsgi_app(environ, start_response):
 if __name__ == '__main__':
     from wsgiref.simple_server import make_server
 
-    httpd = make_server('localhost', 80, wsgi_app)
+    httpd = make_server('localhost', 5555, wsgi_app)
     httpd.serve_forever()
